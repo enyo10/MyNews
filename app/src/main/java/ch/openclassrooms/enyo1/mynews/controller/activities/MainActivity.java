6106df93f048542160943860014f8f25e4c8a1d6
enyo10 <enyo10@yahoo.fr>
2018-12-09 21:34:21 +0100

    second commit

--- app/src/main/java/ch/openclassrooms/enyo1/mynews/controller/activities/MainActivity.java
+++ app/src/main/java/ch/openclassrooms/enyo1/mynews/controller/activities/MainActivity.java
@@ -1,14 +1,12 @@
 package ch.openclassrooms.enyo1.mynews.controller.activities;
 
 import android.support.design.widget.NavigationView;
-import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
