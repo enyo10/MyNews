4368031d8f663f0dd63b9dd51511724f43ec35c7
enyo10 <enyo10@yahoo.fr>
2019-03-05 23:54:05 +0100

    Most popular article class creaction

---
+++ app/src/main/java/ch/openclassrooms/enyo1/mynews/models/mostPopular/Result.java
@@ -0,0 +1,307 @@
+package ch.openclassrooms.enyo1.mynews.models.mostPopular;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class Result {
+    @SerializedName("url")
+    @Expose
+    private String url;
+    @SerializedName("adx_keywords")
+    @Expose
+    private String adxKeywords;
+    @SerializedName("subsection")
+    @Expose
+    private String subsection;
+    @SerializedName("share_count")
+    @Expose
+    private Integer shareCount;
+    @SerializedName("count_type")
+    @Expose
+    private String countType;
+    @SerializedName("column")
+    @Expose
+    private String column;
+    @SerializedName("eta_id")
+    @Expose
+    private Integer etaId;
+    @SerializedName("section")
+    @Expose
+    private String section;
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+    @SerializedName("asset_id")
+    @Expose
+    private Integer assetId;
+    @SerializedName("nytdsection")
+    @Expose
+    private String nytdsection;
+    @SerializedName("byline")
+    @Expose
+    private String byline;
+    @SerializedName("type")
+    @Expose
+    private String type;
+    @SerializedName("title")
+    @Expose
+    private String title;
+    @SerializedName("abstract")
+    @Expose
+    private String _abstract;
+    @SerializedName("published_date")
+    @Expose
+    private String publishedDate;
+    @SerializedName("source")
+    @Expose
+    private String source;
+    @SerializedName("updated")
+    @Expose
+    private String updated;
+    @SerializedName("des_facet")
+    @Expose
+    private List<String> desFacet = null;
+    @SerializedName("org_facet")
+    @Expose
+    private List<String> orgFacet = null;
+    @SerializedName("per_facet")
+    @Expose
+    private String perFacet;
+    @SerializedName("geo_facet")
+    @Expose
+    private String geoFacet;
+    @SerializedName("media")
+    @Expose
+    private List<Medium> media = null;
+    @SerializedName("uri")
+    @Expose
+    private String uri;
+    @SerializedName("format")
+    @Expose
+    private String format;
+    @SerializedName("height")
+    @Expose
+    private Integer height;
+    @SerializedName("width")
+    @Expose
+    private Integer width;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getAdxKeywords() {
+        return adxKeywords;
+    }
+
+    public void setAdxKeywords(String adxKeywords) {
+        this.adxKeywords = adxKeywords;
+    }
+
+    public String getSubsection() {
+        return subsection;
+    }
+
+    public void setSubsection(String subsection) {
+        this.subsection = subsection;
+    }
+
+    public Integer getShareCount() {
+        return shareCount;
+    }
+
+    public void setShareCount(Integer shareCount) {
+        this.shareCount = shareCount;
+    }
+
+    public String getCountType() {
+        return countType;
+    }
+
+    public void setCountType(String countType) {
+        this.countType = countType;
+    }
+
+    public String getColumn() {
+        return column;
+    }
+
+    public void setColumn(String column) {
+        this.column = column;
+    }
+
+    public Integer getEtaId() {
+        return etaId;
+    }
+
+    public void setEtaId(Integer etaId) {
+        this.etaId = etaId;
+    }
+
+    public String getSection() {
+        return section;
+    }
+
+    public void setSection(String section) {
+        this.section = section;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getAssetId() {
+        return assetId;
+    }
+
+    public void setAssetId(Integer assetId) {
+        this.assetId = assetId;
+    }
+
+    public String getNytdsection() {
+        return nytdsection;
+    }
+
+    public void setNytdsection(String nytdsection) {
+        this.nytdsection = nytdsection;
+    }
+
+    public String getByline() {
+        return byline;
+    }
+
+    public void setByline(String byline) {
+        this.byline = byline;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAbstract() {
+        return _abstract;
+    }
+
+    public void setAbstract(String _abstract) {
+        this._abstract = _abstract;
+    }
+
+    public String getPublishedDate() {
+        return publishedDate;
+    }
+
+    public void setPublishedDate(String publishedDate) {
+        this.publishedDate = publishedDate;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(String updated) {
+        this.updated = updated;
+    }
+
+    public List<String> getDesFacet() {
+        return desFacet;
+    }
+
+    public void setDesFacet(List<String> desFacet) {
+        this.desFacet = desFacet;
+    }
+
+    public List<String> getOrgFacet() {
+        return orgFacet;
+    }
+
+    public void setOrgFacet(List<String> orgFacet) {
+        this.orgFacet = orgFacet;
+    }
+
+    public String getPerFacet() {
+        return perFacet;
+    }
+
+    public void setPerFacet(String perFacet) {
+        this.perFacet = perFacet;
+    }
+
+    public String getGeoFacet() {
+        return geoFacet;
+    }
+
+    public void setGeoFacet(String geoFacet) {
+        this.geoFacet = geoFacet;
+    }
+
+    public List<Medium> getMedia() {
+        return media;
+    }
+
+    public void setMedia(List<Medium> media) {
+        this.media = media;
+    }
+
+    public String getUri() {
+        return uri;
+    }
+
+    public void setUri(String uri) {
+        this.uri = uri;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public void setFormat(String format) {
+        this.format = format;
+    }
+
+    public Integer getHeight() {
+        return height;
+    }
+
+    public void setHeight(Integer height) {
+        this.height = height;
+    }
+
+    public Integer getWidth() {
+        return width;
+    }
+
+    public void setWidth(Integer width) {
+        this.width = width;
+    }
+
+}
